07a4c1ff42aeaac6c572ae997d10f6e317181b22
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2024-10-10 16:28:16 +0300

    Time: 35 ms (83.89%), Space: 16.5 MB (54.67%) - LeetHub

--- 1873-longest-nice-substring/1873-longest-nice-substring.py
+++ 1873-longest-nice-substring/1873-longest-nice-substring.py
@@ -1,17 +1,10 @@
 class Solution:
-    def get_nice(self, s: str) -> bool:
-        return len(set(s.lower())) == (len(set(s)) // 2)
-        
     def longestNiceSubstring(self, s: str) -> str:
-        window_size = len(s)
-        
-        while window_size:
-            for i in range(len(s) - window_size + 1):
-                substring = s[i:i + window_size]
-                
-                if self.get_nice(substring):
-                    return substring
-                
-            window_size -= 1
-            
-        return ''
+        if not s: return "" # boundary condition 
+        ss = set(s)
+        for i, c in enumerate(s):
+            if c.swapcase() not in ss: 
+                s0 = self.longestNiceSubstring(s[:i])
+                s1 = self.longestNiceSubstring(s[i+1:])
+                return max(s0, s1, key=len)
+        return s
