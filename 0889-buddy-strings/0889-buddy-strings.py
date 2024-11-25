1fe6319a4aee0407756e2f5a95d63bd26c19635c
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2024-11-25 11:05:50 +0300

    Time: 0 ms (100%), Space: 16.8 MB (18.94%) - LeetHub

--- 0889-buddy-strings/0889-buddy-strings.py
+++ 0889-buddy-strings/0889-buddy-strings.py
@@ -5,16 +5,12 @@ class Solution:
         count = 0
         set1 = set()
         set2 = set() 
-        set3 = set()
         for i in range(len(s)):
             if s[i] != goal[i]:
                 count += 1
                 set1.add(s[i])
                 set2.add(goal[i])
-            else:
-                set3.add(s[i])
-        print(set1 == set2)
-        if count == 2 and set1 == set2 or count == 0 and len(set3) <= len(s) -1:
+        if count == 2 and set1 == set2 or count == 0 and len(set(s)) <= len(s) -1:
             return True
         else:
             return False
