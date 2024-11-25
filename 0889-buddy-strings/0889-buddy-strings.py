fd8f15ddcc6114c14ff59dcb401b0afd75bc8c56
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2024-11-25 11:38:12 +0300

    Time: 3 ms (49.24%), Space: 16.8 MB (18.94%) - LeetHub

--- 0889-buddy-strings/0889-buddy-strings.py
+++ 0889-buddy-strings/0889-buddy-strings.py
@@ -1,7 +1,7 @@
 class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
-        if len(s) != len(goal):
-            return False
+        if len(s) != len(goal):return False
+        if len(set(s)) < len(s) and s == goal:return True
         count = 0
         set1 = set()
         set2 = set() 
@@ -10,7 +10,7 @@ class Solution:
                 count += 1
                 set1.add(s[i])
                 set2.add(goal[i])
-        if count == 2 and set1 == set2 or count == 0 and len(set(s)) <= len(s) -1:
+        if count == 2 and set1 == set2 :
             return True
         else:
             return False
