7fed2696c9166c8744484ad6392f013a93cf49ab
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2024-11-29 11:58:56 +0300

    Time: 15 ms (18.55%), Space: 18.4 MB (38.45%) - LeetHub

--- 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.py
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.py
@@ -1,20 +1,14 @@
 class Solution:
     def sortArrayByParityII(self, nums: List[int]) -> List[int]:
-        even = 0
-        odd = 1
+        even ,odd = 0,1
         while odd < len(nums) and even < len(nums):
-            print(even,odd)
             if not self.iseven(nums[even]) and self.iseven(nums[odd]):
                 nums[odd], nums[even] =  nums[even], nums[odd]
                 even += 2
                 odd += 2
-            elif self.iseven(nums[even]) and  not self.iseven(nums[odd]):
-                even += 2
-                odd += 2
-            elif self.iseven(nums[even]):
-                even += 2
-            elif not self.iseven(nums[odd]):
-                odd += 2
+            else:
+                if self.iseven(nums[even]):even += 2
+                if not self.iseven(nums[odd]):odd += 2  
         return nums
 
     def iseven(self,i):
