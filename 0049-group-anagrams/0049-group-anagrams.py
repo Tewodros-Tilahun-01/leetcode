7fcdb1803b4023ba0da9e106e39f760376f8ecf6
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-06-09 13:32:55 +0300

    Time: 15 ms (50.4%), Space: 21.2 MB (44.17%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         count = {}
-        res = []
         for i in strs:
             if  tuple(sorted(i)) in count:
                 count[  tuple(sorted(i)) ].append(i)
             else:
                 count[  tuple(sorted(i))  ] = [i]
-        for i in count:
-            res.append(count[i])
-        return res
+        return list(count.values())
