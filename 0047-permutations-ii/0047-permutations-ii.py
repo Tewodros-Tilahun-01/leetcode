7656e781adf3ec460eed7afa02a635ae21cae212
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-06-30 19:18:31 +0300

    Time: 139 ms (16.35%), Space: 18.7 MB (5.29%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        
+        def helper(p,up):
+            if len(up) == 0:
+                res.add(tuple(p))
+                return
+            val = up[0]
+            for i in range(len(p)+1):
+                print(1)
+                helper( p[:i] + [ val ]+  p[i:],up[1:])
+        helper([],nums)
+        return list(res)
