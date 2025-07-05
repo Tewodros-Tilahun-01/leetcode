9cdcc3147a14b4b7d9c6847b08dcdf2274aa5e6b
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-07-05 22:46:43 +0300

    Time: 12 ms (5.67%), Space: 18.1 MB (14.74%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,31 +4,22 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        x = 10
-        list1 = []
-        resHead = None
-
-        res = self.add(l1,1)
-        res += self.add(l2,1)
-        
-        if res == 0:
-            resHead = ListNode(0)
-        while res > 0:
-            temp = res % x 
-            list1.append(temp)
-            res = (res - temp)//10
-        list1.reverse()
-       
-        for i in list1:
-            temp = ListNode(i,resHead)
-            resHead = temp
-        return resHead
-
-    def add(self,head,x):
-        if head == None:
-            return 0
-        temp = self.add(head.next,x*10)
-        return temp + (head.val * x)
-        
-        
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:  
+        res = []
+        def rec(node,total,pow):
+            if not node:
+                print(total)
+                return total
+            total += node.val * pow
+            return rec(node.next,total,pow * 10)
+        x = rec(l1,0,1) + rec(l2,0,1)
+        head = ListNode(0)
+        temp = head
+        if x == 0:
+            return ListNode(x)
+        while x != 0:
+            val = x%10
+            x = x//10
+            temp.next = ListNode(val)
+            temp = temp.next
+        return head.next
