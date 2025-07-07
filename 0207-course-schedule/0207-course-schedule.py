8de9ce5c5e05f78b9f2234edcddbc62a61348ef2
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-07-07 15:10:53 +0300

    Time: 9 ms (18.22%), Space: 20 MB (10.39%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -2,23 +2,28 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         ref = defaultdict(list)
-        path = set()
-        visited = set() 
         for course, prereq in prerequisites:
             ref[course].append(prereq)
-
+        
+        path = set()
+        visited = set()
+        
         def dfs(cor):
-            if cor in path:return False
-            if cor in visited:return True
+            if cor in path:
+                return False
+            if cor in visited:
+                return True
             path.add(cor)
             for prereq in ref[cor]:
-                if not dfs(prereq):return False
+                if not dfs(prereq):
+                    return False
             path.remove(cor)
             visited.add(cor)
             return True
         
         for course in range(numCourses):
             if course not in visited:
-                if not dfs(course):return False
+                if not dfs(course):
+                    return False
         
         return True
