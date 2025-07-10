77610462c335263ccddf1d9a20da3fda012f7365
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-07-10 13:38:33 +0300

    Time: 0 ms (100%), Space: 17.7 MB (95.36%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,14 +1,12 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        def perm(p,up):
-            nonlocal res
+        def perm(p,up,res): 
             if not up:
                 res.append(p)
-                return
+                return res
             num = up[0]
             for i in range(len(p)+1):
-                perm(p[:i] + [num] + p[i:],up[1:])
-        perm( [ nums[0] ], nums[1:] )
-        return res
+               perm(p[:i] + [num] + p[i:],up[1:],res)
+            return res
+        return perm( [ nums[0] ] , nums[1:] ,[] )
    
