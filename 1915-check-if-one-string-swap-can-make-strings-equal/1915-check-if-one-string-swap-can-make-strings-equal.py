25facdaa267d3e6980cc2d7cd6e8fec2076556ff
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-07-17 23:42:36 +0300

    Time: 0 ms (100%), Space: 17.9 MB (15.36%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
@@ -1,14 +1,16 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        count = 0
-        set1 = set()
-        set2 = set()
+        if s1 == s2:
+            return True
+        swap = 0
+        hashmap1 = set()
+        hashmap2 = set()
+
         for i in range(len(s1)):
             if s1[i] != s2[i]:
-                count += 1
-                set1.add(s1[i])
-                set2.add(s2[i])
-        if count not in [0,2] or set1 != set2:
-            return False
-        else:
+                swap += 1
+                hashmap1.add(s1[i])
+                hashmap2.add(s2[i])
+        if swap == 2 and hashmap1 == hashmap2:
             return True
+        return False
