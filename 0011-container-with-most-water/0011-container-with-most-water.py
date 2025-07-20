fccfb0b3b9689806a702f37a2f4772b7f8909218
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-07-20 23:53:11 +0300

    Time: 111 ms (21.54%), Space: 28.4 MB (67.26%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,22 +1,15 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        left = 0
-        right = len(height) - 1
-        max_water = 0
-        
-        while left < right:
-            # Calculate current area
-            width = right - left
-            current_height = min(height[left], height[right])
-            current_area = width * current_height
-            
-            # Update max_water if current_area is larger
-            max_water = max(max_water, current_area)
-            
-            # Move the pointer with smaller height
-            if height[left] < height[right]:
-                left += 1
+        ptr1 = max_capacity = 0
+        ptr2 = width = len(height) - 1
+         
+        while ptr2 > ptr1:
+            capacity = min(height[ptr1],height[ptr2]) * width
+            max_capacity = max(max_capacity , capacity)
+            if height[ptr1] < height[ptr2]:
+                ptr1 += 1
             else:
-                right -= 1
-                
-        return max_water
+                ptr2 -= 1
+            width -= 1
+
+        return max_capacity
