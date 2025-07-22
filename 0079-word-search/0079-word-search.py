7c40bd13501847a9b5d945a375aad81f3f8c320f
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-07-22 23:41:00 +0300

    Time: 4502 ms (41.87%), Space: 17.9 MB (65.03%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,28 +3,27 @@ class Solution:
         visted = set()
         dir = [[0,1],[0,-1],[1,0],[-1,0]]
         
-        def backtrack(board,index,row,col):
+        def backtrack(index,row,col):
             if index == len(word):
                 return True
 
-            flag = False
             for r , c in dir:
                 nx = r + row 
                 ny = c + col
                 if 0 <= nx < len(board) and 0 <= ny < len(board[0]) \
                 and word[index] == board[nx][ny] and (nx,ny) not in visted:
                     visted.add((nx,ny)) 
-                    if backtrack(board,index+1,nx,ny):
-                        flag = True
+                    if backtrack(index+1,nx,ny):
+                        return True
                     visted.remove((nx,ny))
-            return flag
+            return False
             
 
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == word[0] :
                     visted.add((i,j))
-                    if backtrack(board,1,i,j):
+                    if backtrack(1,i,j):
                         return True
                     visted.remove((i,j))
         return False
