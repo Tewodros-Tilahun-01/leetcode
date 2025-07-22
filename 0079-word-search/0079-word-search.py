d3a29e7ded7ae443ffd6371dc08ac5c1e55bb2f2
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-07-22 23:48:07 +0300

    Time: 4370 ms (48.46%), Space: 18 MB (39.85%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -10,21 +10,22 @@ class Solution:
             for r , c in dir:
                 nx = r + row 
                 ny = c + col
+
                 if 0 <= nx < len(board) and 0 <= ny < len(board[0]) \
                 and word[index] == board[nx][ny] and (nx,ny) not in visted:
+
                     visted.add((nx,ny)) 
-                    if backtrack(index+1,nx,ny):
-                        return True
+                    if backtrack(index+1,nx,ny):return True
                     visted.remove((nx,ny))
+
             return False
             
 
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if board[i][j] == word[0] :
+                if board[i][j] == word[0]:
                     visted.add((i,j))
-                    if backtrack(1,i,j):
-                        return True
+                    if backtrack(1,i,j):return True
                     visted.remove((i,j))
         return False
 
