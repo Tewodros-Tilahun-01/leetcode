da306aa2f34574477d8db1221d21f010919d0e0d
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-07-24 23:40:06 +0300

    Time: 3 ms (80.05%), Space: 18.8 MB (79.17%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,15 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        first_zero = -1
+        ptr = 0
         for i in range(len(nums)):
-            if nums[i] == 0:
-                first_zero = i
-                break
-        if first_zero == -1:
-            return 
-        for i in range(len(nums)):
-            if nums[i] != 0 and i > first_zero:
-                nums[first_zero] , nums[i] = nums[i],nums[first_zero]
-                first_zero += 1
-        
+            if nums[i] != 0 :
+                nums[ptr] = nums[i]
+                ptr += 1
+        for i in range(ptr,len(nums)):
+            nums[i] = 0
