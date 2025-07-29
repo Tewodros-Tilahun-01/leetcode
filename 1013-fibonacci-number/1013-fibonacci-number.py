4cf5d20a0a24a42683659f5e9a7a028a05e49a61
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-07-29 21:12:54 +0300

    Time: 1448 ms (5.13%), Space: 17.7 MB (40.8%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,5 +1,11 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n < 2:
-            return n
-        return self.fib(n - 1) + self.fib(n - 2)
+        memo = {}
+        def rec(memo,n):
+            if n < 2:
+                return n
+            if n in memo:
+                return memo[n]
+            memo[n] = self.fib(n-1) + self.fib(n-2)
+            return memo[n]
+        return rec(memo,n)
