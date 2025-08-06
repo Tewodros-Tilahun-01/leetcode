cd485651249e47edb58d609bb6bf00ae6fdb13c3
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-08-06 20:46:57 +0300

    Time: 0 ms (100%), Space: 17.9 MB (28.73%) - LeetHub

---
+++ 1137-height-checker/1137-height-checker.py
@@ -0,0 +1,8 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+        not_match = 0
+        for i in range(len(heights)):
+            if expected[i] != heights[i]:
+                not_match += 1
+        return not_match
