6c029804216ac4b2483b14167ed387a7c70f0a6b
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-08-11 23:18:03 +0300

    Time: 3 ms (29.75%), Space: 17.8 MB (62.98%) - LeetHub

---
+++ 0306-additive-number/0306-additive-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+        
+        def backtrack(path,i):
+            if i == len(num) and len(path) > 2:
+                return True
+            
+            for j in range(i,len(num)):
+                val = num[i:j+1]
+                if (val[0] != "0" or len(val) == 1):
+                    if len(path) < 2 or path[-1] + path[-2] == int(val):
+                        if backtrack(path+[int(val)],j+1):
+                            return True
+            return False
+        return backtrack([],0)           
+
+
+        
+            
