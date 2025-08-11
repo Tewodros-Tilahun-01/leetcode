673f2709ccb1dc78b5f652877dd3479f264dffdf
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-08-11 17:48:26 +0300

    Time: 1 ms (71.41%), Space: 17.8 MB (75.82%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,20 +1,29 @@
 class Solution:
-    def restoreIpAddresses(self, s: str) -> List[str]:
-        res = [] 
-        def backtrack(ip,i):
-            if i == len(s) and len(ip) == 4:
-                res.append(  ".".join( map(str,ip) ) ) 
-                return 
-            if len(ip) > 3:
-                return 
+    def restoreIpAddresses(self, s: str) -> list[str]:
+        res = []
+        n = len(s)
 
-            for j in range(i,len(s)):
-                st = "".join(s[i:j+1]) 
-                val = int(st)
-                if val <= 255 and len(st) == len(str(val)):
-                    ip.append(val)
-                    backtrack(ip,j+1)
-                    val = ip.pop()       
-        ip = []
-        backtrack(ip,0)
+        def backtrack(start: int, path: list[str]):
+            print(path)
+            # If we've formed 4 segments and used all chars
+            if len(path) == 4:
+                if start == n:
+                    res.append(".".join(path))
+                return
+
+            # Try segments of length 1 to 3
+            for length in range(1, 4):
+                if start + length > n:
+                    break
+                segment = s[start:start+length]
+                
+                # Skip invalid segments
+                if (segment[0] == "0" and length > 1) or int(segment) > 255:
+                    continue
+                
+                path.append(segment)
+                backtrack(start + length, path)
+                path.pop()
+
+        backtrack(0, [])
         return res
