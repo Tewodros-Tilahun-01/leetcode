ee9456339680516a47cc83e6e157f9687f78dcd1
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-08-17 23:50:57 +0300

    Time: 40 ms (71.19%), Space: 18.3 MB (27.76%) - LeetHub

---
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        graph = collections.defaultdict(list)
+        q = collections.deque()
+        res = []
+        
+        def change_to_graph(graph,node):
+            if not node:
+                return
+            if node.right:
+                graph[node.val].append(node.right.val)
+                graph[node.right.val].append(node.val)
+            if node.left:
+                graph[node.val].append(node.left.val)
+                graph[node.left.val].append(node.val)
+            change_to_graph(graph,node.left)
+            change_to_graph(graph,node.right)
+        change_to_graph(graph,root)
+
+        q.append((target.val,0))
+        visited = set()
+        while q:
+            val , step = q.popleft()  
+            visited.add(val)  
+            for nb in graph[val]:
+                if nb not in visited and step <= k:
+                    q.append((nb,step+1))
+            if step == k:
+                res.append(val)
+        return res
+        
