8409d2e7313145267fb4a7335d0146648e3680e6
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-08-19 21:25:45 +0300

    Time: 1159 ms (17.1%), Space: 45.9 MB (7.26%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,14 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         memo = {}
-        def dp(n,count):
-            if n <= 0:
-                return 0 if n == 0 else amount
-            if n in memo:return memo[n]
-            min_coin = float("inf")
+        
+        def dp(n):
+            if n < 0:
+                return float("inf") 
+            if n == 0:
+                return 0  
+            if n in memo:
+                return memo[n]
+            
+            min_coins = float("inf")
             for coin in coins:
-                min_coin = min(min_coin,1 + dp(n-coin,count+1))
-            memo[n] = min_coin
+                min_coins = min(min_coins, 1 + dp(n - coin))
+            memo[n] = min_coins
             return memo[n]
-        res = dp(amount,1) 
-        return res if res <= amount else -1
+        
+        result = dp(amount)
+        return result if result != float("inf") else -1
