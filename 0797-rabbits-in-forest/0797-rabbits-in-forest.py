aeafdcbd2b1d9cb4de69b84d0e055b6e84c7edfb
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-08-27 23:58:19 +0300

    Time: 0 ms (100%), Space: 18 MB (26.86%) - LeetHub

---
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        answers.sort()
+        res = 0
+        answered = {}
+        for i in answers:
+            if i not in answered or i == 0:
+                res += 1 + i
+                answered[i] = i
+            elif answered[i] > 0:
+                answered[i] -= 1 
+            else:
+                answered[i] = i 
+                res += 1 + i
+        return res
