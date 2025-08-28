f1a3ff8c7750d115fe4f1920060ddef9666acc2f
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-08-28 19:10:09 +0300

    Time: 84 ms (42.66%), Space: 19.6 MB (76.01%) - LeetHub

---
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        sup = set(supplies)
+        graph = collections.defaultdict(list)
+        incoming = collections.defaultdict(int)
+        queue = collections.deque(supplies)
+        toposort = []
+        for i in range(len(recipes)):
+            for j in ingredients[i]:
+                graph[j].append(recipes[i])
+                incoming[recipes[i]] += 1        
+        while queue:
+            ig = queue.popleft()
+            for recipe in graph[ig]:
+                incoming[recipe] -= 1
+                if incoming[recipe] == 0 and recipe not in sup:
+                    toposort.append(recipe)
+                    queue.append(recipe)
+        return toposort
+        
