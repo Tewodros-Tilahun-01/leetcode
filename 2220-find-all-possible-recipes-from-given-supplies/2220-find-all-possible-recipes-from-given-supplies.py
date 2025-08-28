28496a88c12e10a4850e8ce376af8e126dbe28d1
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-08-28 19:19:48 +0300

    Time: 105 ms (28.26%), Space: 19.8 MB (54.07%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.py
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.py
@@ -7,13 +7,14 @@ class Solution:
         toposort = []
         for i in range(len(recipes)):
             for j in ingredients[i]:
-                graph[j].append(recipes[i])
-                incoming[recipes[i]] += 1        
+                if recipes[i] not in sup:
+                    graph[j].append(recipes[i])
+                    incoming[recipes[i]] += 1        
         while queue:
             ig = queue.popleft()
             for recipe in graph[ig]:
                 incoming[recipe] -= 1
-                if incoming[recipe] == 0 and recipe not in sup:
+                if incoming[recipe] == 0:
                     toposort.append(recipe)
                     queue.append(recipe)
         return toposort
