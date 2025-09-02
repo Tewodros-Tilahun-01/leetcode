66082596ce30fa1bc4fba9652ff0bd6d7a210adf
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-09-02 13:05:17 +0300

    Time: 32 ms (78.11%), Space: 30 MB (49.7%) - LeetHub

--- 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.py
+++ 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.py
@@ -1,17 +1,19 @@
 class Solution:
     def minimumReplacement(self, nums: List[int]) -> int:
-        n = len(nums)
-        last = nums[n - 1]  # Initialize 'last' with the last element
-        ans = 0  # Initialize the total operations count
+        current_largest = nums[-1]
+        total_replacements = 0
+        
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] <= current_largest:
+                current_largest = nums[i]
+                continue
 
-        # Traverse the array in reverse order
-        for i in range(n - 2, -1, -1):
-            if nums[i] > last:  # If the current element needs replacement
-                t = nums[i] // last  # Calculate how many times the element needs to be divided
-                if nums[i] % last:
-                    t += 1  # If there's a remainder, increment 't'
-                last = nums[i] // t  # Update 'last' for the next comparison
-                ans += t - 1  # Add (t - 1) to 'ans' for the number of operations
+            if nums[i] % current_largest:
+                num_elements = nums[i] // current_largest + 1
+                current_largest = nums[i] // num_elements
             else:
-                last = nums[i]  # Update 'last' without replacement
-        return ans  # Return the total number of operations
+                num_elements = nums[i] // current_largest
+            
+            total_replacements += num_elements - 1
+        
+        return total_replacements
