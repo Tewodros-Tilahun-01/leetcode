5685b32054d5ddcf69e4bdcb897ec939de974c6c
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-09-03 21:51:37 +0300

    Time: 7 ms (51.6%), Space: 18.1 MB (7.31%) - LeetHub

---
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        parent = {}
+        
+        def find(x):
+            if x not in parent:
+                parent[x] = x
+            if x != parent[x]:
+                parent[x] = find(parent[x])
+            return parent[x]
+        def union(x,y):
+            px = find(x)
+            py = find(y)
+            if px > py:
+                px , py = py , px
+            parent[py] = px
+        n = len(s1)
+        for i in range(n):
+            union(s1[i],s2[i])
+        
+        res = []
+        for s in baseStr:
+            res.append(find(s))
+        return "".join(res)
+                
