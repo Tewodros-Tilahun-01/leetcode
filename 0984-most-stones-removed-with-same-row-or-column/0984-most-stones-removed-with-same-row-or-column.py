737885e9f16a2b4c0898eb791ba8102d1b3287df
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-09-03 18:33:29 +0300

    Time: 19 ms (74.65%), Space: 18.5 MB (62.16%) - LeetHub

---
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,29 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        parent = {}
+        rank = {}
+        
+        def find(x):
+            if x not in parent:
+                parent[x] = x
+                rank[x] = 0
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        
+        def union(x, y):
+            px, py = find(x), find(y)
+            if px == py:
+                return
+            if rank[px] < rank[py]:
+                px, py = py, px
+            parent[py] = px
+            if rank[px] == rank[py]:
+                rank[px] += 1
+        
+        for x, y in stones:
+            row, col = x, y + 10001
+            union(row, col)
+        
+        groups = set(find(x) for x, y in stones)
+        return len(stones) - len(groups)
