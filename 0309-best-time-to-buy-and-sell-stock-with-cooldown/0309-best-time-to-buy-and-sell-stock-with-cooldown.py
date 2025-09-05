650cacb53227cc520c5248b9765663b36ede2f2c
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-09-05 21:23:16 +0300

    Time: 7 ms (42.07%), Space: 19 MB (30.13%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -2,18 +2,19 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
         memo = {}
-        
-        def dp(i):
+
+        def dp(i, can_buy):
             if i >= n:
                 return 0
-            if i in memo:
-                return memo[i]
-            best = dp(i + 1)
-        
-            for j in range(i + 1, n):
-                if prices[j] > prices[i]:
-                    best = max(best, prices[j] - prices[i] + dp(j + 2))
-            memo[i] = best
-            return memo[i]
-        
-        return dp(0)
+            if (i, can_buy) in memo:
+                return memo[(i, can_buy)]
+
+            if can_buy:
+                profit = max(-prices[i] + dp(i + 1, False),dp(i + 1, True))
+            else:
+                profit = max(prices[i] + dp(i + 2, True),dp(i + 1, False))
+
+            memo[(i, can_buy)] = profit
+            return profit
+
+        return dp(0, True)
