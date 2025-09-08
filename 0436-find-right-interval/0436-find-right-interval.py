da189db10b69030fdd32469bcd1e42a8e3bf0158
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-09-08 19:10:34 +0300

    Time: 56 ms (45.08%), Space: 22.2 MB (27.5%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.py
+++ 0436-find-right-interval/0436-find-right-interval.py
@@ -1,35 +1,26 @@
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
-        n = len(intervals)
         change_to_index = {}
-        for index ,value in enumerate(intervals):
-            start ,end = value
+        for index, value in enumerate(intervals):
+            start, _ = value
             change_to_index[start] = index
-
-        intervals.sort(reverse=True)
-        def find(start,prvStart):
-            change_to_index[start]
-        res = [0] * n
+        
+        intervals.sort()
+        
+        n = len(intervals)
+        res = [-1] * n
+        
         for i in range(n):
-            start , end = intervals[i]
-            original = change_to_index[start]
-            if start == end:
-                res[original] = original 
-            elif i == 0:
-                res[original] = -1
-            else:
-                j = i - 1
-                prvstart = None
-                while j >= 0:
-                    prvstart = intervals[j][0]
-                    if prvstart >= end:
-                        break
-                    j -= 1
-                res[original] = change_to_index[prvstart] if j >= 0 else -1
-        return res
+            start, end = intervals[i]
+            original_index = change_to_index[start]
+            left, right = 0, n
+            while left < right:
+                mid = (left + right) // 2
+                if intervals[mid][0] >= end:
+                    right = mid
+                else:
+                    left = mid + 1
+            if left < n:
+                res[original_index] = change_to_index[intervals[left][0]]
                 
-            
-        
-
-
-        
+        return res
