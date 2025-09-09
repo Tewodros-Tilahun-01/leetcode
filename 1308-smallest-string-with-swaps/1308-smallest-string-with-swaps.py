0f49381783e6151a1f4fd465bd3f4453b150e3c3
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-09-09 10:35:09 +0300

    Time: 190 ms (13.4%), Space: 46.7 MB (54.42%) - LeetHub

---
+++ 1308-smallest-string-with-swaps/1308-smallest-string-with-swaps.py
@@ -0,0 +1,36 @@
+class Solution:
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        n = len(s)
+        parent = {}
+        connected = defaultdict(list)
+        def find(x):
+            if x not in parent:
+                parent[x] = x
+            if x != parent[x]:
+                parent[x] = find(parent[x])
+            return parent[x]
+        def union(x,y):
+            px = find(x)
+            py = find(y)
+            if px != py:
+                if s[px] < s[py]:
+                    parent[py] = px
+                else:
+                    parent[px] = py
+
+        for x , y in pairs:
+            union(x,y)
+        
+        for i in range(n):
+            connected[find(i)].append(s[i])
+        
+        for key ,value in connected.items():
+            connected[key] = sorted(value,reverse=True)
+        print(connected)
+        
+        res = []
+        for i in range(n):
+            char = connected[find(i)].pop()
+            res.append(char)
+            
+        return "".join(res)
