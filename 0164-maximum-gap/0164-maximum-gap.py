9889394c07d97d8097def1d78bff858506eef0f7
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-09-12 17:23:19 +0300

    Time: 703 ms (6.81%), Space: 30.4 MB (30.84%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -1,40 +1,41 @@
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n < 2:
+        if len(nums) < 2:
             return 0
-
-        def counting_sort(arr,exp):
-            nonlocal n
-            count = [0] * 10
-            temp_res = [0] * n
-
-            for i in range(n):
-                digit = arr[i] // exp
-                count[digit % 10] += 1
             
-            for i in range(1,10):
-                count[i] = count[i] + count[i-1]
+        def counting_sort(arr, exp):
+            n = len(arr)
+            count = [0] * 10
+            output = [0] * n
+            
+            for num in arr:
+                digit = (num // exp) % 10
+                count[digit] += 1
+            
+            for i in range(1, 10):
+                count[i] += count[i-1]
+            
 
-            for i in range(n-1,-1,-1):
-                digit = arr[i] // exp
-                temp_res [ count[digit % 10 ] - 1 ] = arr[i]
-                count[digit % 10] -= 1
+            for num in arr[::-1]:
+                digit = (num // exp) % 10
+                count[digit] -= 1
+                output[count[digit]] = num
+            
 
-            arr[:] = temp_res
-
-
-        def redix_sort(arr):
-            max_element = max(arr)
+            arr[:] = output
+        
+        def radix_sort(arr):
+            max_num = max(arr)
             exp = 1
-            while max_element // exp:
-                counting_sort(arr,exp)
+            while max_num // exp:
+                counting_sort(arr, exp)
                 exp *= 10
-            return arr
-
-        redix_sort(nums)
+        
+        radix_sort(nums)
+        
+        
         res = 0
-        for i in range(1,n):
-            res = max(res , nums[i]-nums[i-1])
+        for i in range(1, len(nums)):
+            res = max(res, nums[i] - nums[i-1])
         
         return res
