6d47b492b00462cd53dcd6a8a617f4b1863f0709
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-09-16 20:47:08 +0300

    Time: 69 ms (71.19%), Space: 18.9 MB (53.64%) - LeetHub

---
+++ 2609-distinct-prime-factors-of-product-of-array/2609-distinct-prime-factors-of-product-of-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def distinctPrimeFactors(self, nums: List[int]) -> int:
+        
+        def prime_factors(num,hashmap):
+            d = 2
+            while d * d <= num:
+                if num % d == 0:
+                    hashmap.add(d)
+                    num = num // d
+                else:
+                    d += 1
+            if d > 1:
+                hashmap.add(num)
+
+        hashmap = set()
+        for num in nums:
+            if num < 2:continue
+            prime_factors(num,hashmap)
+        
+        return len(hashmap)
+            
+            
