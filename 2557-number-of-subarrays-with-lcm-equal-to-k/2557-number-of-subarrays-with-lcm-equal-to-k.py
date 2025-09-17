79cb7c6bb0b34c8d615935db2a991a2e73d4704e
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-09-17 20:45:00 +0300

    Time: 156 ms (86.03%), Space: 17.8 MB (61.03%) - LeetHub

--- 2557-number-of-subarrays-with-lcm-equal-to-k/2557-number-of-subarrays-with-lcm-equal-to-k.py
+++ 2557-number-of-subarrays-with-lcm-equal-to-k/2557-number-of-subarrays-with-lcm-equal-to-k.py
@@ -1,22 +1,13 @@
 class Solution:
     def subarrayLCM(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        count = 0
+        cnt = 0
 
-        def gcf(m,n):
-            if n == 0:return m
-            return gcf(n,m%n)
-
-        def lcm(n,m):
-           return( m * n )  // gcf(m,n)      
-
-        for i in range(n):
-            _lcm = nums[i]
-            for j in range(i,n):
-                _lcm = lcm(_lcm,nums[j]) 
-                if _lcm == k:
-                    count += 1
-                elif _lcm > k:
-                    break
+        for i in range(0, len(nums)):
+            l = nums[i]            
+            for j in range(i, len(nums)):  
+                l = lcm(l,nums[j])          
+                if l == k : cnt += 1       
+                if l > k  : break          
+            
+        return cnt
         
-        return count
