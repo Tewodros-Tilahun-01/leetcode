b86b85b67bbd490501dc36d318299af38c1bc6c5
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-09-18 23:00:14 +0300

    Time: 191 ms (33.48%), Space: 18.6 MB (73.09%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.py
+++ 0661-image-smoother/0661-image-smoother.py
@@ -3,18 +3,17 @@ class Solution:
         dir = [(1,0),(-1,0),(0,1),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1)]
         rows , cols = len(img) , len(img[0])
         res = [[0]*cols for _ in range(rows)]
-        
+
         def is_valid(row,col):
             return 0 <= row < rows and 0 <= col < cols
 
         for i in range(rows):
-            for j in range(cols):
-                total = img[i][j] 
-                nums = 1
+            for j in range(cols):     
+                total , nums = img[i][j] , 1
+                
                 for dx , dy in dir:
-                    row ,col = i + dx , j + dy
-                    if is_valid(row,col):
-                        total += img[row][col]
+                    if is_valid(i+dx,j+dy):
+                        total += img[i+dx][j+dy]
                         nums += 1
                 res[i][j] = total // nums
         
