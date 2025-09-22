b182abcb6da5c41f4e335ca9e5ab28560729b7c6
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-09-22 19:39:18 +0300

    Time: 6292 ms (23.3%), Space: 499.5 MB (22.8%) - LeetHub

---
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self):
+        self.kids = [None, None]  # 0 and 1
+
+class Solution:
+    def findMaximumXOR(self, arr: List[int]) -> int:
+        root = Node()
+        
+        # Insert all numbers into the Trie first
+        for n in arr:
+            cur = root
+            for i in range(31, -1, -1):
+                b = (n >> i) & 1
+                if not cur.kids[b]:
+                    cur.kids[b] = Node()
+                cur = cur.kids[b]
+        
+        # Query for maximum XOR
+        ans = 0
+        for n in arr:
+            cur = root
+            val = 0
+            for i in range(31, -1, -1):
+                b = (n >> i) & 1
+                t = 1 - b
+                if cur.kids[t]:
+                    val |= (1 << i)
+                    cur = cur.kids[t]
+                else:
+                    cur = cur.kids[b]
+            ans = max(ans, val)
+        
+        return ans
