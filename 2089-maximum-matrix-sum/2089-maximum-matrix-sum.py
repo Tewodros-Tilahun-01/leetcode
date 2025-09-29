4c91714bfa79ba2ec13f995ff522a4bfd602f953
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-09-29 23:00:39 +0300

    Time: 108 ms (14.01%), Space: 25.7 MB (21.74%) - LeetHub

--- 2089-maximum-matrix-sum/2089-maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.py
@@ -3,19 +3,16 @@ class Solution:
         neg = 0
         small = float("inf")
         res = 0
-        zero = False
-
+        
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if matrix[i][j] == 0:
-                    zero = True 
-                elif matrix[i][j] < 0:
+                if matrix[i][j] < 0:
                     neg += 1                
                     matrix[i][j] *= -1
                 small = min(small,abs(matrix[i][j]))
                 res += matrix[i][j]
                 
-        if neg % 2 == 1 and not zero:
+        if neg % 2 == 1:
             res -= (2 * small)
             
         return res
