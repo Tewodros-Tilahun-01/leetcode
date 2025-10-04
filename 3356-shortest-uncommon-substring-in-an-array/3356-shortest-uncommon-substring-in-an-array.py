469bb8e86508b20af8ced42eb87765e25f6d186e
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-10-04 21:22:05 +0300

    Time: 402 ms (18.7%), Space: 21 MB (10.32%) - LeetHub

--- 3356-shortest-uncommon-substring-in-an-array/3356-shortest-uncommon-substring-in-an-array.py
+++ 3356-shortest-uncommon-substring-in-an-array/3356-shortest-uncommon-substring-in-an-array.py
@@ -1,7 +1,7 @@
 class TrieNode:
     def __init__(self):
-        self.children = {} 
-        self.indices = set()  
+        self.children = {}
+        self.indices = set()
 
 class Trie:
     def __init__(self):
@@ -13,59 +13,49 @@ class Trie:
             if char not in node.children:
                 node.children[char] = TrieNode()
             node = node.children[char]
-        node.indices.add(index)  
+        node.indices.add(index)
     
     def get_indices(self, substr: str) -> set:
         node = self.root
         for char in substr:
             if char not in node.children:
-                return set()  
+                return set()
             node = node.children[char]
         return node.indices
 
 class Solution:
     def shortestSubstrings(self, arr: List[str]) -> List[str]:
-        trie = Trie()
-
         n = len(arr)
+        trie = Trie()
+        
+        # Insert all substrings into the Trie
         for i in range(n):
             string = arr[i]
             m = len(string)
-            for length in range(1,m+1):
-                for start in range(m-length+1):
-                    substr = string[start:start+length]
-                    trie.insert(substr,i)
-
+            for length in range(1, m + 1):
+                for start in range(m - length + 1):
+                    substr = string[start:start + length]
+                    trie.insert(substr, i)
+        
         res = [""] * n
         for i in range(n):
             string = arr[i]
             m = len(string)
+            best_substr = None
             
-            for length in range(1,m+1):
-                substrings = []
-                for start in range(m-length+1):
-                    substr = string[start:start+length]
-                    substrings.append(substr)
-
-                substrings.sort()
-                for sub in substrings:
-                    if len(trie.get_indices(sub)) < 2:
-                        res[i] = sub
-                        break
-                if res[i]:
+            # Try each length starting from 1
+            for length in range(1, m + 1):
+                # Check each substring of this length
+                for start in range(m - length + 1):
+                    substr = string[start:start + length]
+                    indices = trie.get_indices(substr)
+                    if len(indices) == 1 and i in indices:
+                        # Found a unique substring
+                        if best_substr is None or substr < best_substr:
+                            best_substr = substr
+                # If a valid substring is found, no need to check longer lengths
+                if best_substr:
+                    res[i] = best_substr
                     break
         
         return res
-
-
-                
-            
-            
-
-
-
-
-
-
-            
-        
