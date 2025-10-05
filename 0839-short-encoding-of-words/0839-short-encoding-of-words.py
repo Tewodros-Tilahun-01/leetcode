30ded6d51533afdc8fcd9ba2efb9553933098a2e
tewodros tilahun <113050664+Tewodros-Tilahun-01@users.noreply.github.com>
2025-10-05 12:36:10 +0300

    Time: 99 ms (32.43%), Space: 19.9 MB (28.96%) - LeetHub

--- 0839-short-encoding-of-words/0839-short-encoding-of-words.py
+++ 0839-short-encoding-of-words/0839-short-encoding-of-words.py
@@ -1,40 +1,28 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
+from typing import List
 
-    def insert(self,word):
-        node = self.root
-        for c in word:
-            if c not in node.children:
-                node.children[c] = TrieNode()
-            node = node.children[c] 
-            
-    def prifix(self,word):
-        node = self.root
-        for c in word:
-            if c not in node.children:
-                return False
-            node = node.children[c] 
-        return True
-
-        
 class Solution:
     def minimumLengthEncoding(self, words: List[str]) -> int:
-        trie = Trie()
-        res = 0
+        class TrieNode:
+            def __init__(self):
+                self.children = {}
+                self.word_len = 0  # Store length of word ending here
 
-        words.sort(key=lambda x:-(len(x)))
-
+        root = TrieNode()
         for word in words:
-            rev = word[::-1]
-            if not trie.prifix(rev):
-                trie.insert(rev)
-                res += len(word) + 1
+            node = root
+            rword = word[::-1]
+            for char in rword:
+                if char not in node.children:
+                    node.children[char] = TrieNode()
+                node = node.children[char]
+            node.word_len = len(word)  # Mark word length at leaf
 
-        return res
-        
-        
-        
+        def dfs(node, depth):
+            total = 0
+            if not node.children and node.word_len > 0:
+                total += node.word_len + 1  # Add word length + '#'
+            for child in node.children.values():
+                total += dfs(child, depth + 1)
+            return total
+
+        return dfs(root, 0)
